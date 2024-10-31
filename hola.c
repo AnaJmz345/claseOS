619bd509c581f94e72759d979171ac253c22d25d
AnaJmz345 <143899647+AnaJmz345@users.noreply.github.com>
2024-10-31 14:48:57 +0000

    hola 3 nombres

--- hola.c
+++ hola.c
@@ -2,7 +2,7 @@
 
 int main(int argc,char **argv){ //argc es el número de elementos
     for(int i=0;i<argc;i=i+1){
-         printf("Hola mundo %s\n",argv[i]);
+         printf("Hola %s\n",argv[i]);
     }
    
 }
